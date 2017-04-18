ba64f210d9ec0858c2c8a2bca107b473444baf90
Paul Franz <paulkfranz@gmail.com>
2017-04-18 11:35:03 -0400

    iniital commit and sheet setup

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1F3edu2tYpU6ey52ZL4xlFSb2XJiEQZmBg4Ymlju4z-w/edit#gid=0';
